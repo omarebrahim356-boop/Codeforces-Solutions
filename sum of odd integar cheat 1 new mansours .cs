34279dbc928163a337b862a6749da30515186501
Omar Ebrahim Metwaly <omarebrahim356@gmail.com>
2026-06-09 01:08:57 +0300

    Create sum of odd integar cheat 1 new mansours .cs

---
+++ sum of odd integar cheat 1 new mansours .cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System;
+namespace ConsoleApp95
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cheat one new mansoura sum of odd integers
+            int t = int.Parse(Console.ReadLine()!);
+            while( t--  > 0)
+            {
+               long[] num = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
+                long n = num[0];
+                long k = num[1];
+                Console.WriteLine(n % 2 == k % 2 && n >= k * k ? "YES" : "NO");
+            }
+        }
+        
+    }
+}
